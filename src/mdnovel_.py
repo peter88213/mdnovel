9a4f602bb8d92e53e690ce5befc614b119a3d993
peter88213 <petert88213@gmail.com>
2025-09-04 13:46:41 +0200

    Update mdnovel_.py

--- src/mdnovel_.py
+++ src/mdnovel_.py
@@ -2,7 +2,7 @@
 """A novel writing application using markdown.
 
 Version @release
-Requires Python 3.6+
+Requires Python 3.7+
 Copyright (c) 2024 Peter Triesberger
 For further information see https://github.com/peter88213/mdnovel
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
